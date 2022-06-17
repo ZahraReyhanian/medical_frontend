6a4ca6df701db2c9d2c5a54b2f15eaf3109cf0a5
Zahra Reyhanian <reyhanyanz@gmail.com>
2022-06-17 13:29:34 +0430

    change TestSection

--- src/components/sections/TestSection.js
+++ src/components/sections/TestSection.js
@@ -80,19 +80,4 @@ const ButtonWrapper = styled.div`
   text-align: left;
 `;
 
-const TestButton = styled(Button)`
-  border-radius: 20px;
-  width: 13rem;
-  height: 3.2rem;
-  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
-    0px 20px 40px rgba(23, 0, 102, 0.2),
-    inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
-  transition: 1s 0.2s cubic-bezier(0.075, 0.82, 0.165, 1);
-
-  :hover {
-    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
-      0px 30px 60px rgba(23, 0, 102, 0.5),
-      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
-    transform: translateY(-3px);
-  }
-`;
+const TestButton = styled(Button)``;
