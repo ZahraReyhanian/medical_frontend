6a49399389f83b451348b0bd095acb86fe8a8cde
Zahra Reyhanian <reyhanyanz@gmail.com>
2022-07-21 22:00:45 +0430

    create ProfilrSetting

--- src/pages/profile/ProfileSetting.js
+++ src/pages/profile/ProfileSetting.js
@@ -5,46 +5,80 @@ import Title from "./Title";
 import styled from "styled-components";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { toast } from "react-toastify";
-// import { getUserPanel, updateProfileSetting } from "../../api/api_home";
+import useAxiosAuth from "../../hooks/useAxiosAuth";
+
+const isText = RegExp(/^[A-Z ]+$/i);
+const isEmail = RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i);
 
 const ProfileSetting = () => {
   const [email, setEmail] = useState();
-  const [name, setName] = useState();
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
   const [username, setUsername] = useState();
-  const [weight, setWeight] = useState();
-  const [height, setHeight] = useState();
-  const [age, setAge] = useState();
-  const [gender, setGender] = useState();
-  const [problem, setProblem] = useState();
+
+  const [user, setUser] = useState({});
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  let api = useAxiosAuth();
+
+  const getData = async (url) => {
+    let data = {};
+    try {
+      let response = await api.get(url);
+      data = response.data;
+      console.log(data);
+      setUser(data);
+      setEmail(data.email);
+      setUsername(data.username);
+      setFirstName(data.first_name);
+      setLastName(data.last_name);
+    } catch (err) {
+      console.log(err.message);
+
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    // getUserPanel((isOk, data) => {
-    //   if (!isOk) return alert(data.message);
-    //   else {
-    //     const user = data.user;
-    //     const student = data.user.student[0];
-    //     setEmail(user.email);
-    //     setName(user.name);
-    //     setWeight(student.weight);
-    //     setHeight(student.Height);
-    //     setAge(student.age);
-    //     setGender(student.gender);
-    //     setProblem(student.medicalSpecifications);
-    //   }
-    // });
+    const url = "/auth/users/me/";
+
+    getData(url);
   }, []);
 
+  const validation = (user_values) => {
+    if (!user_values.email) return "ایمیل را وارد کنید";
+    if (!isEmail.test(user_values.email)) return "ایمیل وارد شده صحیح نمی باشد";
+    if (!user_values.username) return "نام کاربری را وارد کنید";
+  };
+
+  const submit_setting = async (url, user_values) => {
+    try {
+      let response = await api.patch(url, user_values);
+      const data = response.data;
+      console.log(data);
+      toast.success("با موفقیت ثبت شد");
+    } catch (err) {
+      console.log(err.message);
+
+      setError(err.message);
+    } finally {
+    }
+  };
+
   const handleProfile = () => {
-    // const user = {
-    //   email: email,
-    //   name: name,
-    //   weight: weight,
-    //   Height: height,
-    //   age: age,
-    //   gender: gender,
-    //   medicalSpecifications: problem,
-    //   sick: "false",
-    // };
+    const user_values = {
+      username: username,
+      email: email,
+      first_name: firstName,
+      last_name: lastName,
+    };
+    const error = validation(user_values);
+    if (error) return toast.warn(error);
+    submit_setting("/auth/users/me/", user_values);
+
     // if (problem != "") user.sick = "true";
     // updateProfileSetting(user, (isOk, data) => {
     //   if (!isOk) return toast.error(data);
@@ -61,47 +95,63 @@ const ProfileSetting = () => {
     <PersonalWrapper>
       <Row>
         <Col lg={12} md={12} sm={12}>
-          <Title title="Personal Setting" span="" />
+          <Title title="تنظیمات" span="" />
         </Col>
       </Row>
-      <AccountRow>
-        <Col lg={12} md={12} sm={12}>
-          <Form>
-            <Form.Group className="mb-5" controlId="formBasicEmail">
-              <Form.Label>ایمیل</Form.Label>
-              <Form.Control
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                placeholder="ایمیل را وارد کنید"
-              />
-            </Form.Group>
-            <Form.Group className="mb-5" controlId="formBasicName">
-              <Form.Label>نام کاربری</Form.Label>
-              <Form.Control
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                type="text"
-                placeholder="نام کاربری را وراد کنید"
-              />
-            </Form.Group>
+      {loading && <p>در حال بارگزاری ...</p>}
 
-            <Form.Group className="mb-5" controlId="formBasicName">
-              <Form.Label>نام و نام خانوادگی</Form.Label>
-              <Form.Control
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                type="text"
-                placeholder="نام و نام خانوادگی"
-              />
-            </Form.Group>
+      {!loading && error && <p className="errMsg">{error}</p>}
 
-            <ButtonWrapper>
-              <SettingButton onClick={handleProfile}>ثبت</SettingButton>
-            </ButtonWrapper>
-          </Form>
-        </Col>
-      </AccountRow>
+      {!loading && !error && user && (
+        <AccountRow>
+          <Col lg={12} md={12} sm={12}>
+            <Form>
+              <Form.Group className="mb-5" controlId="formBasicEmail">
+                <Form.Label>ایمیل</Form.Label>
+                <Form.Control
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  placeholder="ایمیل را وارد کنید"
+                />
+              </Form.Group>
+              <Form.Group className="mb-5" controlId="formBasicName">
+                <Form.Label>نام کاربری</Form.Label>
+                <Form.Control
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  type="text"
+                  placeholder="نام کاربری را وراد کنید"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-5" controlId="formBasicName">
+                <Form.Label>نام</Form.Label>
+                <Form.Control
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  type="text"
+                  placeholder="نام"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-5" controlId="formBasicName">
+                <Form.Label>نام خانوادگی</Form.Label>
+                <Form.Control
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  type="text"
+                  placeholder="نام خانوادگی"
+                />
+              </Form.Group>
+
+              <ButtonWrapper>
+                <SettingButton onClick={handleProfile}>ثبت</SettingButton>
+              </ButtonWrapper>
+            </Form>
+          </Col>
+        </AccountRow>
+      )}
     </PersonalWrapper>
   );
 };
